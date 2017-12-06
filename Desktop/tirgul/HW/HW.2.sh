7f2ada5505c42ca66bafc5607647c2837621d722
Dor shamay <33579173+CentoShamay@users.noreply.github.com>
2017-12-06 16:47:32 +0200

    Update and rename HW.2.AB.sh to HW.2.sh

--- Desktop/tirgul/HW/HW.2.sh
+++ Desktop/tirgul/HW/HW.2.sh
@@ -5,7 +5,7 @@ read -p "Please enter the credentials digits " mod1
 read -p "Are you sure you want to make these changes? Y/N " ans
 if [ $ans = y ]
 then
-	mkdir $mod; chmod $mod1 $mod;
+	mkdir $mod; chmod -v -c $mod1 $mod; ls -lc |grep $mod;
 else
 	echo "Script was Discarded"
 fi
